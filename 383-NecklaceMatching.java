54bb1c57aa92fcc8725ad28cfc546d74cb3bb893
priyasha-g <99101410+priyasha-g@users.noreply.github.com>
2022-02-10 13:04:37 +1100

    Update 383-NecklaceMatching.java

--- 383-NecklaceMatching.java
+++ 383-NecklaceMatching.java
@@ -1,29 +1,40 @@
 public class Main
 {
 	public static void main(String[] args) {
-System.out.println(same_necklace("abc", "cba"));
-
+        System.out.println(repeats("abc"));
+        System.out.println(same_necklace("nicole", "lenico"));
 	}
 	
-	public static String modify(String str1){
-	    char[] strArray = str1.toCharArray();
+	// BONUS
+	public static int repeats(String str){
+	    String originalStr = str;
+	    int repeats = 0;
+	    
+	    for (int i=0; i<str.length(); i++){
+	       str = modify(str);
+	       if (str.equals(originalStr)){
+	           repeats++;
+	       }
+	    }
+	   
+	    return repeats;
+	}
+	
+	public static String modify(String str){
+	    char[] strArray = str.toCharArray();
 	    char temp = strArray[0];
 	    
-	    for (int i=1; i<str1.length(); i++){
+	    for (int i=1; i<str.length(); i++){
 	        strArray[i-1] = strArray[i];
 	    }
 	    
 	    strArray[strArray.length - 1] = temp;
         
-        String newStr1 = new String(strArray);
-        return newStr1;
+        String newStr = new String(strArray);
+        return newStr;
 	}
 	
 	public static boolean same_necklace(String str1, String str2){
-	   char[] strArray = str1.toCharArray();
-	   if (str1 == str2){
-	       return true;
-	   }
 	   
 	   for (int i=0; i<str1.length(); i++){
 	       
